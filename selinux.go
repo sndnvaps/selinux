47ae3c860cce09ad5e556610bc28dfef8fb74683
sndnvaps <sndnvaps@gmail.com>
2014-07-09 16:42:28 +0800

    Add SECURITY_CAPABILITY, SECURITY_SELINUX to deal  with Lgetxattr() and Lsetxattr()  use standary library syscall to get SELINUX_LABEL or security.capability from system if the system support
     New func list
    	func Lgetxattr(path string, attr string) ([]byte, error)
    	func Lsetxattr(path string, attr string, data []byte, flags int)
    error
    	func Fgetxattr(fd uintptr, attr string) ([]byte, error)
    	func Fsetxattr(fd uintptr, attr string, data []byte, flags int)
    error
    
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- selinux.go
+++ selinux.go
@@ -39,7 +39,9 @@ import (
 	"regexp"
 	//"io"
 	//"os"
+	//"strconv"
 	"strings"
+	"syscall"
 )
 
 var (
@@ -47,7 +49,108 @@ var (
 	mcs_list    = make(map[string]bool)
 )
 
+const (
+	SECURITY_CAPABILITY = "security.capability" // for get capability from xattr
+	SECURITY_SELINUX    = "security.selinux"    //for get selinux_label from xattr
+)
+
 // C.mode_t
+
+// Returns a nil slice and nil error if the xattr is not set
+func Lgetxattr(path string, attr string) ([]byte, error) {
+	pathBytes, err := syscall.BytePtrFromString(path)
+	if err != nil {
+		return nil, err
+	}
+	attrBytes, err := syscall.BytePtrFromString(attr)
+	if err != nil {
+		return nil, err
+	}
+	dest := make([]byte, 128)
+	destBytes := unsafe.Pointer(&dest[0])
+	sz, _, errno := syscall.Syscall6(syscall.SYS_LGETXATTR, uintptr(unsafe.Pointer(pathBytes)), uintptr(unsafe.Pointer(attrBytes)), uintptr(destBytes), uintptr(len(dest)), 0, 0)
+	if errno == syscall.ENODATA {
+		return nil, nil
+	}
+	if errno == syscall.ERANGE {
+		dest = make([]byte, sz)
+		destBytes := unsafe.Pointer(&dest[0])
+		sz, _, errno = syscall.Syscall6(syscall.SYS_LGETXATTR, uintptr(unsafe.Pointer(pathBytes)), uintptr(unsafe.Pointer(attrBytes)), uintptr(destBytes), uintptr(len(dest)), 0, 0)
+	}
+	if errno != 0 {
+		return nil, errno
+	}
+
+	return dest[:sz], nil
+}
+
+var _zero uintptr
+
+func Lsetxattr(path string, attr string, data []byte, flags int) error {
+	pathBytes, err := syscall.BytePtrFromString(path)
+	if err != nil {
+		return err
+	}
+	attrBytes, err := syscall.BytePtrFromString(attr)
+	if err != nil {
+		return err
+	}
+	var dataBytes unsafe.Pointer
+	if len(data) > 0 {
+		dataBytes = unsafe.Pointer(&data[0])
+	} else {
+		dataBytes = unsafe.Pointer(&_zero)
+	}
+	_, _, errno := syscall.Syscall6(syscall.SYS_LSETXATTR, uintptr(unsafe.Pointer(pathBytes)), uintptr(unsafe.Pointer(attrBytes)), uintptr(dataBytes), uintptr(len(data)), uintptr(flags), 0)
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
+
+//os.File.Fd() uintptr
+func Fgetxattr(fd uintptr, attr string) ([]byte, error) {
+	attrBytes, err := syscall.BytePtrFromString(attr)
+	if err != nil {
+		return nil, err
+	}
+	dest := make([]byte, 128)
+	destBytes := unsafe.Pointer(&dest[0])
+	sz, _, errno := syscall.Syscall6(syscall.SYS_FGETXATTR, fd, uintptr(unsafe.Pointer(attrBytes)), uintptr(destBytes), uintptr(len(dest)), 0, 0)
+	if errno == syscall.ENODATA {
+		return nil, nil
+	}
+	if errno == syscall.ERANGE {
+		dest = make([]byte, sz)
+		destBytes := unsafe.Pointer(&dest[0])
+		syscall.Syscall6(syscall.SYS_FSETXATTR, fd, uintptr(unsafe.Pointer(attrBytes)), uintptr(destBytes), uintptr(len(dest)), 0, 0)
+	}
+
+	if errno != 0 {
+		return nil, errno
+	}
+
+	return dest[:sz], nil
+}
+
+func Fsetxattr(fd uintptr, attr string, data []byte, flags int) error {
+
+	attrBytes, err := syscall.BytePtrFromString(attr)
+	if err != nil {
+		return err
+	}
+	var dataBytes unsafe.Pointer
+	if len(data) > 0 {
+		dataBytes = unsafe.Pointer(&data[0])
+	} else {
+		dataBytes = unsafe.Pointer(&_zero)
+	}
+	_, _, errno := syscall.Syscall6(syscall.SYS_FSETXATTR, fd, uintptr(unsafe.Pointer(attrBytes)), uintptr(dataBytes), uintptr(len(data)), uintptr(flags), 0)
+	if errno != 0 {
+		return errno
+	}
+	return nil
+}
 
 func GetMode_t(path string) (C.mode_t, int) {
 	var st C.struct_stat
--- selinux_test.go
+++ selinux_test.go
@@ -4,13 +4,13 @@ package selinux_test
 
 import (
 	"fmt"
+	se "github.com/sndnvaps/selinux"
 	"os"
-	. "selinux"
 	"testing"
 )
 
 func TestSelinux_enabled(t *testing.T) {
-	if Selinux_enabled() {
+	if se.Selinux_enabled() {
 		fmt.Println("SELinux status = Enabled\n")
 	} else {
 		fmt.Println("SELinux status = Disabled\n")
@@ -20,7 +20,7 @@ func TestSelinux_enabled(t *testing.T) {
 func TestGetfilecon(t *testing.T) {
 	var flabel string
 	var size int
-	flabel, size = Lgetfilecon("selinux.go")
+	flabel, size = se.Lgetfilecon("selinux.go")
 	if size > 0 {
 		fmt.Println("selinux.go label = ", flabel)
 	}
@@ -29,7 +29,7 @@ func TestGetfilecon(t *testing.T) {
 func TestSetfilecon(t *testing.T) {
 	path := "selinux.go"
 	scon := "system_u:object_r:usr_t:s0"
-	rc, _ := Lsetfilecon(path, scon)
+	rc, _ := se.Lsetfilecon(path, scon)
 	if rc != 0 {
 		fmt.Println("Setfilecon failed\n")
 	} else {
@@ -48,7 +48,7 @@ func TestFsetfilecon(t *testing.T) {
 	fd := int(f.Fd())
 
 	scon := "system_u:object_r:usr_t:s0"
-	rc, _ := Fsetfilecon(fd, scon)
+	rc, _ := se.Fsetfilecon(fd, scon)
 	if rc != 0 {
 		fmt.Println("fsetfilecon failed\n")
 	} else {
@@ -58,9 +58,9 @@ func TestFsetfilecon(t *testing.T) {
 
 func TestMatchpathcon(t *testing.T) {
 	path := "selinux_test.go"
-	mode, ecode := GetMode_t(path)
+	mode, ecode := se.GetMode_t(path)
 	if ecode == 0 {
-		con, err := Matchpathcon(path, mode)
+		con, err := se.Matchpathcon(path, mode)
 		if err != nil {
 			fmt.Println("selinux_test.go selabel = ", con)
 		}
@@ -69,20 +69,84 @@ func TestMatchpathcon(t *testing.T) {
 
 func TestSelinux_getenforcemode(t *testing.T) {
 	var enforce int
-	enforce = Selinux_getenforcemode()
+	enforce = se.Selinux_getenforcemode()
 	fmt.Printf("%s", "Selinux mode = ")
-	if enforce == Enforcing {
+	if enforce == se.Enforcing {
 		fmt.Println("Enforcing mode\n")
-	} else if enforce == Permissive {
+	} else if enforce == se.Permissive {
 		fmt.Println("permissive mode\n")
-	} else if enforce == Disabled {
+	} else if enforce == se.Disabled {
 		fmt.Println("Disabled mode\n")
 	}
 }
 func TestGetPidcon(t *testing.T) {
 	pid := os.Getpid()
-	fmt.Printf("PID:%d MCS:%s\n", pid, IntToMcs(pid, 1023))
-	if scon, err := Getpidcon(pid); err == nil {
+	fmt.Printf("PID:%d MCS:%s\n", pid, se.IntToMcs(pid, 1023))
+	if scon, err := se.Getpidcon(pid); err == nil {
 		fmt.Printf("pid = %d, security_context = %s ", pid, scon)
 	}
+}
+
+func TestLgetxattr(t *testing.T) {
+	fc, err := os.OpenFile("se_xtars_test.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+	defer fc.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fc_xattr, err := se.Lgetxattr(fc.Name(), se.SECURITY_SELINUX)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("se_xattrs_test.txt xattr = ", string(fc_xattr))
+	}
+
+}
+
+func TestLsetxattr(t *testing.T) {
+	fc, err := os.OpenFile("se_xtars_test.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+	defer fc.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	scon := "system_u:object_r:usr_t:s0"
+	err = se.Lsetxattr(fc.Name(), se.SECURITY_SELINUX, []byte(scon), 0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("set ", fc.Name(), "selinux_label -> ", scon, "success")
+	}
+
+}
+
+func TestFgetxattr(t *testing.T) {
+	fc, err := os.OpenFile("se_xtars_test_fd.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+	defer fc.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fc_xattr, err := se.Fgetxattr(fc.Fd(), se.SECURITY_SELINUX)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("TestFgetxattr:se_xattrs_test.txt xattr = ", string(fc_xattr))
+	}
+
+}
+
+func TestFsetxattr(t *testing.T) {
+	fc, err := os.OpenFile("se_xtars_test_fd.txt", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
+	defer fc.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	scon := "system_u:object_r:usr_t:s0"
+
+	err = se.Fsetxattr(fc.Fd(), se.SECURITY_SELINUX, []byte(scon), 0)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("TestFsetxattr:se_xattrs_test_fd.txt xattr = ", scon)
+	}
+
 }
